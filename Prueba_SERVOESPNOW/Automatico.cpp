511953acf75f425f43cdecf482730f14b7c741dc
Rodrigo <rodrigo.garcia.pascual@gmail.com>
2024-01-01 20:59:26 +0100

    Cambios ligeros en modo auto

--- Prueba_SERVOESPNOW/Automatico.cpp
+++ Prueba_SERVOESPNOW/Automatico.cpp
@@ -4,17 +4,26 @@
 //sensor ir -> No detecta = 1
 void plantaAutomatica()
 {
+  unsigned long int t;
 	int clasificacion[3] = { 1, 2, 3 };
-	while (digitalRead(sensorIR) == 1)
+	while (digitalRead(sensorIR) == 0)
 	{
-		Motor(200);
+		Motor(100);
+    vTaskDelay(1);
 	}
 
-	for (int i = 0; i <= 20; i++)
-	{
-		Motor(140);
+	unsigned long int tactual = millis();
+  t = millis();
+  while(t-tactual < 1000)
+  {
+		Motor(70);
+    t = millis();
+    vTaskDelay(1);
 	}
-
+  // if(t - tactual > 1000)
+  // {
+  //   Motor(0);
+  // }
 	//A�adir aqui el sistema de identificacion y clasificacion
 	for (int j = 0; j < sizeof(clasificacion); j++)
 	{
